b11c408fd206cc6e35e859660d7e36d0d95a51c7
Ana Hinojosa <ahinojosasanchez@gmail.com>
2024-02-14 12:54:18 +0100

    p1 completed - not working because of error in genome screenshot with WSL2

--- p1_screenshot.py
+++ p1_screenshot.py
@@ -1,6 +1,31 @@
+# It doesn't work on WSL2 Windows for problems gnome capturer!!!!!!!!!!!!
+
+
 import time
-import pyautogui
+import tkinter as tk
+
+# import pyautogui
+
 
 def screenshot():
-    time.sleep(5)
-    img=pyautogui.screenshot('test.png')
+    name = int(round(time.time() * 1000))
+    name = "/home/ahinojosa/formacion/20-python-projects/screenshots/{}.png".format(
+        name
+    )
+    print("Screenshot saved at:", name)
+    # time.sleep(5)
+    # img = pyautogui.screenshot("name")
+    # img.show()
+
+
+root = tk.Tk()
+frame = tk.Frame(root)
+frame.pack()
+
+button = tk.Button(frame, text="Text a screenshot", command=screenshot)
+button.pack(side=tk.LEFT)
+
+close = tk.Button(frame, text="Quit", command=quit)
+close.pack(side=tk.LEFT)
+
+root.mainloop()
